df145b0a80e245a7a96a655ceceb44cbf23a3c2f
leonleerl <leon.runlongli@gmail.com>
2025-05-20 02:02:29 +0800

    feat: category GET and DELETE

--- src/components/admin/categories/categories-table.tsx
+++ src/components/admin/categories/categories-table.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
     Table,
     TableBody,
@@ -6,33 +7,119 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-    Button
+    Button,
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    Input,
+    Form,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormControl,
+    FormMessage
   } from "@/components/ui"
-  
-  const invoices = [
-    {
-      id: "INV001",
-      name: "C#",
-    },
-    {
-      id: "INV002",
-      name: "Java",
-    },
-    {
-      id: "INV003",
-      name: "Python",
-    },
-    {
-      id: "INV004",
-      name: "JavaScript",
-    },
-    
-  ]
-  
+import { CategoryFormSchema, CategoryFormSchemaType } from "@/schemas/category.schema";
+import { CategoryDto } from "@/types/category";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 function CategoriesTable() {
-    return (
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
+  const [categories, setCategories] = useState<CategoryDto[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [currentCategory, setCurrentCategory] = useState<CategoryDto>();
+  const router = useRouter();
+
+
+  const [editName, setEditName] = useState<string>("");
+
+  const form = useForm<CategoryFormSchemaType>({
+    resolver: zodResolver(CategoryFormSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch('/api/category');
+      const result = await response.json();
+      setCategories(result.data);
+    } catch (err) {
+      setError(err as Error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const handleDeleteCategory = async (category: CategoryDto) => {
+
+    try {
+      const response = await fetch(`/api/category/${category.id}`, {
+        method: 'DELETE',
+      });
+      
+      if (response.ok) {
+        fetchCategories();
+      } else {
+        const errorData = await response.json();
+        alert(`Failed to delete: ${errorData.message || 'Unknown error'}`);
+      }
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+      alert(`Error: ${errorMessage}`);
+      console.error(err);
+    }
+
+  }
+
+  const handleUpdateCategory = async (formData: CategoryFormSchemaType) => {
+    try {
+      const response = await fetch(`/api/category/${formData.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(formData.name),
+      });
+
+      if (response.ok) {
+        router.push('/admin/categories');
+      }
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+      alert(`Error: ${errorMessage}`);
+      console.error(err);
+    }
+  }
+
+  const handleEditDialogOpen = (category: CategoryDto) => {
+    setCurrentCategory(category);
+    setEditName(category.name);
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className="w-[100px]">Id</TableHead>
@@ -41,13 +128,65 @@ function CategoriesTable() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.id}>
-              <TableCell className="font-medium">{invoice.id}</TableCell>
-              <TableCell className="font-medium">{invoice.name}</TableCell>
+          {categories?.map((category) => (
+            <TableRow key={category.id}>
+              <TableCell className="font-medium">{category.id}</TableCell>
+              <TableCell className="font-medium">{category.name}</TableCell>
               <TableCell className="font-medium">
-                <Button variant="default">Edit</Button>
-                <Button variant="destructive">Delete</Button>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant="outline" onClick={() => handleEditDialogOpen(category)}>Edit</Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                      <DialogTitle>Edit</DialogTitle>
+                      <DialogDescription>
+                        Make changes to your profile here. Click save when you&apos;re done.
+                      </DialogDescription>
+                    </DialogHeader>
+                        <Form {...form}>
+                        <form onSubmit={form.handleSubmit(handleUpdateCategory)} className="space-y-6">
+                          <div className="grid gap-4 py-4">
+                            <div className="grid items-center gap-4">
+                              <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                  <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                      <Input {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                  </FormItem>
+                                )}
+                              />
+                            </div>
+                          </div>
+                        </form>
+                      </Form>
+                    <DialogFooter>
+                      <Button onClick={() => handleUpdateCategory(category)}>Save changes</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button variant="destructive">Delete</Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Are you sure you want to delete {category.name}?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently delete the data from the server.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={() => handleDeleteCategory(category)}>Delete</AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
               </TableCell>
             </TableRow>
           ))}
