8072a3e0b51442085c6237cffbe5307c3037dda2
leonleerl <leon.runlongli@gmail.com>
2025-05-20 22:25:15 +0800

    refactor: category code refactor

---
+++ src/components/admin/categories/DeleteCategoryDialog.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { 
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button
+} from "@/components/ui";
+import { CategoryDto } from "@/types/category";
+
+interface DeleteCategoryDialogProps {
+  category: CategoryDto;
+  onDelete: (id: string) => Promise<boolean>;
+}
+
+export default function DeleteCategoryDialog({ category, onDelete }: DeleteCategoryDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive">Delete</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to delete {category.name}?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete the data from the server.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => onDelete(category.id)}>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+} 
